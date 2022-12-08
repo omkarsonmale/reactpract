9f482580edfb8efea8ac10e1673f763ab1f1c8e0
Omkar Sonmale <omkars_t20057@students.isquareit.edu.in.com>
2022-12-08 10:10:29 +0530

    New repo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//About JSX 
+// JSX stands for JavaScript XML.
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// JSX allows us to write HTML in React.
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// JSX makes it easier to write and add HTML in React.
+
+// var react=require('react');
+// //import React  from 'react';
+//  var ReactDOM=require('react-dom');
+//  var CreateDOM=require('react-dom')
+// //import React from 'react-dom';
+// //first method
+// ReactDOM.render(<react.Fragment> <h1 id='h1'>KYA DEKHANA HAI</h1> <p> byee</p></react.Fragment> ,document.getElementById('root'))
+// //second method
+// //react.fragment works same like div
+// CreateDOM.render([ <h1 id='h1'>KYA DEKHANA HAI</h1> ,
+// <p> byee</p>,
+// <h3>byee</h3> ],document.getElementById('root'))
+
+//----------------------//First Task---------------------------------------------------------
+// var react=require('react')
+// var ReactDOM=require('react-dom')
+// ReactDOM.render(<react.Fragment>
+//   <h1>Thapa technical netflix pick</h1>
+ 
+//   <p>List of 5 Best series</p>
+ 
+
+//   <ol>
+//            <li>First</li>
+//            <li>second</li>
+//            <li>Third</li>
+//            <li>Fourth</li>
+//            <li>Fifth</li>
+//   </ol>
+// </react.Fragment>,document.getElementById('root'))
+//-------------------------Expression in React-------------------------------------------
+//We write javascript inside that we write HTML code and then if we want to write javascript inside HTML wE USE Expressions
+//javascript->HTML->Javascript
+//In curly bracket we can only write Expression /we Curly bracket use for Expression
+// var React=require('react')
+// var ReactDOM=require('react-dom')
+// var CeactDOM=require('react-dom')
+// const name='omkar'
+// const lname='sonmale'
+// ReactDOM.render(<h1>Hi my name is {name} { } {lname}</h1>,document.getElementById('root'))
+// CeactDOM.render(<h1>Hi my name is {name +" " +lname}</h1>,document.getElementById('root2'))
+//------------Template literals-----------------
+//Not Cleared
+// Example:-console.log('hi i am ${omkar})
+// var React=require('react')
+// var ReactDOM=require('react-dom')
+
+// const name='omkar';
+// const lname='sonmale';
+// ReactDOM.render(<><h1> Hi my name is  {name } {lname}</h1> </>,document.getElementById('root'))
+//-------------------2nd Task-----------------------
+var react=require('react')
+var ReactDOM=require('react-dom')
+
+const name='omkar'
+const Currentdate=new Date().toLocaleDateString();
+const Currenttime=new Date().toLocaleTimeString();
+ReactDOM.render(<div><h1>Hi ,My name is {name} </h1>
+   <h3>Today Date is {Currentdate}</h3>
+   <h3>Current time is {Currenttime}</h3></div>,document.getElementById('root'))
