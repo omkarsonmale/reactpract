f4111ac6002193819ddf00384ae47a7d9103a0d1
Omkar Sonmale <omkars_t20057@students.isquareit.edu.in.com>
2022-12-09 11:22:27 +0530

    adding some

--- src/index.js
+++ src/index.js
@@ -56,12 +56,25 @@
 // const lname='sonmale';
 // ReactDOM.render(<><h1> Hi my name is  {name } {lname}</h1> </>,document.getElementById('root'))
 //-------------------2nd Task-----------------------
-var react=require('react')
-var ReactDOM=require('react-dom')
+// var react=require('react')
+// var ReactDOM=require('react-dom')
 
-const name='omkar'
-const Currentdate=new Date().toLocaleDateString();
-const Currenttime=new Date().toLocaleTimeString();
-ReactDOM.render(<div><h1>Hi ,My name is {name} </h1>
-   <h3>Today Date is {Currentdate}</h3>
-   <h3>Current time is {Currenttime}</h3></div>,document.getElementById('root'))
+// const name='omkar'
+// const Currentdate=new Date().toLocaleDateString();
+// const Currenttime=new Date().toLocaleTimeString();
+// ReactDOM.render(<div><h1>Hi ,My name is {name} </h1>
+//    <h3>Today Date is {Currentdate}</h3>
+//    <h3>Current time is {Currenttime}</h3></div>,document.getElementById('root'))
+//------------------Studying the jsx attributes-------------------
+// using contentEditable i'm editing the content
+
+// var react=require('react')
+// var ReactDOM=require('react-dom')
+// var img="https://i.pinimg.com/474x/4a/69/56/4a6956101fbb4910e5eab2a6311185a9.jpg"
+//  ReactDOM.render(<div><h1 ContentEditable='true'>Now i'm editing the text using this</h1><a href='https://www.google.co.in/imghp' target='blank'>
+//  <img src ={img} id='immg'/> </a> </div>,document.getElementById('root'))
+import React from "react";
+import ReactDOM  from "react-dom";
+import './index.css'
+
+ReactDOM.render(<div><h1 >Hello </h1> <fieldset className="field">Tech Road map</fieldset> </div>,document.getElementById('root'))
